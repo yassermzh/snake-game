4db2ba6e32d3537d629962d31e6043540ce8de5d
Yasser Zadeh <yassermzh@gmail.com>
2019-07-16 23:39:12 +0200

    fewer mutations

--- index.js
+++ index.js
@@ -1,89 +1,132 @@
-let vx, vy, px, py, snake, tails, apple;
-let ctx, gameInterval;
-
 const boardSize = 300;
 const tiles = 20;
 const tileSize = boardSize / tiles;
 
-window.onload = function() {
-  const canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const startGame = () => {
+  ctx.fillStyle = "lightgrey";
+  ctx.fillRect(0, 0, boardSize, boardSize);
+
+  let state = {
+    tails: 5,
+    snake: [[0, 0]],
+    apple: newApple(),
+    vx: 1,
+    vy: 0
+  };
+
+  const update = createUpdate();
+
+  document.addEventListener("keydown", event => {
+    const [vx, vy] = keyPush(event);
+    state.vx = vx;
+    state.vy = vy;
+  });
+
+  const loop = () => {
+    const nextState = game(state);
+    if (isCollision(nextState.snake)) {
+      return endGame();
+    }
+    update(nextState);
+    state = nextState;
+    setTimeout(loop, 100);
+  };
+
+  loop();
+};
+
+const endGame = () => {
+  alert("Game Over!");
   startGame();
 };
 
-function startGame() {
-  ctx.fillStyle = "lightgrey";
-  ctx.fillRect(0, 0, boardSize, boardSize);
-  [vx, vy, px, py] = [1, 0, 10, 10];
-  tails = 5;
-  snake = [];
-  newApple();
-  document.addEventListener("keydown", keyPush);
-  gameInterval = setInterval(game, 100);
-}
-
-function drawTile({ px, py }, color) {
+const drawTile = ([x, y], color) => {
   ctx.fillStyle = color;
-  ctx.fillRect(
-    px * tileSize + 1,
-    py * tileSize + 1,
-    tileSize - 1,
-    tileSize - 1
-  );
-}
+  ctx.fillRect(x * tileSize + 1, y * tileSize + 1, tileSize - 1, tileSize - 1);
+};
 
-function endGame() {
-  clearInterval(gameInterval);
-  alert("Game Over!");
-  startGame();
-}
+const newApple = () => [
+  Math.floor(Math.random() * tiles),
+  Math.floor(Math.random() * tiles)
+];
+const eatApple = newApple;
 
-function newApple() {
-  apple = {
-    px: Math.floor(Math.random() * tiles),
-    py: Math.floor(Math.random() * tiles)
+const head = snake => snake[snake.length - 1];
+const tail = snake => snake[0];
+
+const move = state => {
+  const [x, y] = head(state.snake);
+  let [nextX, nextY] = [x + state.vx, y + state.vy];
+  if (nextX >= tiles) nextX = 0;
+  if (nextX < 0) nextX = tiles;
+  if (nextY >= tiles) nextY = 0;
+  if (nextY < 0) nextY = tiles;
+  return limitSnake([...state.snake, [nextX, nextY]], state.tails);
+};
+
+const limitSnake = (snake, tails) =>
+  snake.length > tails ? snake.slice(1) : snake;
+
+const isSameTile = (tile1, tile2) =>
+  tile1[0] === tile2[0] && tile1[1] === tile2[1];
+
+const isCollision = snake => {
+  const [h, ...s] = [...snake].reverse();
+  return s.reduce((acc, tile) => acc || isSameTile(h, tile), false);
+};
+
+const isApple = (snake, apple) => isSameTile(head(snake), apple);
+
+const game = state => {
+  const { apple, tails } = state;
+  let nextState = { ...state, snake: move(state) };
+  if (isApple(nextState.snake, apple)) {
+    nextState = { ...nextState, apple: eatApple(), tails: tails + 1 };
+  }
+  return nextState;
+};
+
+const diff = (snake1, snake2 = []) => {
+  if (snake2.length === 0) return [{ tile: head(snake1), type: "add" }];
+  return [
+    {
+      cond: !isSameTile(tail(snake1), tail(snake2)),
+      change: { tile: tail(snake2), type: "delete" }
+    },
+    {
+      cond: !isSameTile(head(snake1), head(snake2)),
+      change: { tile: head(snake1), type: "add" }
+    }
+  ]
+    .filter(x => x.cond)
+    .reduce((acc, { change }) => [...acc, change], []);
+};
+
+const createUpdate = () => {
+  let cache = { snake: [], apple: [] };
+  return state => {
+    diff(state.snake, cache.snake).forEach(({ tile, type }) => {
+      drawTile(tile, type === "add" ? "grey" : "lightgrey");
+    });
+    if (!isSameTile(state.apple, cache.apple)) drawTile(state.apple, "black");
+    cache = state;
   };
-  drawTile(apple, "cyan");
-}
+};
 
-function game() {
-  if (snake.length >= tails) {
-    const tail = snake.shift();
-    drawTile(tail, "lightgrey");
-  }
-  [px, py] = [px + vx, py + vy];
-  if (px >= tiles) px = 0;
-  if (px < 0) px = tiles;
-  if (py >= t) py = 0;
-  if (py < 0) py = tiles;
-
-  const collision = snake.reduce(
-    (acc, s) => acc || (s.px == px && s.py == py),
-    false
-  );
-  if (collision) endGame();
-  snake.push({ px, py });
-  drawTile({ px, py }, "black");
-
-  if (px === apple.px && py === apple.py) {
-    newApple();
-    tails++;
-  }
-}
-
-function keyPush(evt) {
+const keyPush = evt => {
   switch (evt.keyCode) {
     case 37:
-      [vx, vy] = [-1, 0];
-      break;
+      return [-1, 0];
     case 38:
-      [vx, vy] = [0, -1];
-      break;
+      return [0, -1];
     case 39:
-      [vx, vy] = [1, 0];
-      break;
+      return [1, 0];
     case 40:
-      [vx, vy] = [0, 1];
-      break;
+      return [0, 1];
   }
-}
+};
+
+window.onload = startGame;
